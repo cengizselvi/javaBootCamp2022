fbef0f56ce9c1e1ae61b9193578922968bef7d07
Cengiz Selvi <56098690+cengizselvi@users.noreply.github.com>
2022-09-28 18:37:17 +0300

    2.hafta
    2.hafta

---
+++ Ders2 Ödevleri/reCapDemo_Classes/src/main/java/reCapDemo_Classes/DortISlem.java
@@ -0,0 +1,19 @@
+
+package reCapDemo_Classes;
+
+
+public class DortISlem {
+public int topla(int sayi1,int sayi2) {
+    return sayi1-sayi2;
+}
+public int Cikart(int sayi1,int sayi2) {
+    return sayi1-sayi2;
+}
+public int Carp(int sayi1,int sayi2) {
+    return sayi1*sayi2;
+}
+public int Bol(int sayi1,int sayi2) {
+    return sayi1/sayi2;
+}    
+
+}
